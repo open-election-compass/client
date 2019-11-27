ff1daf38f0cab69336b0f5d6f0bfaaae78a2fdd2
Till Sanders <mail@till-sanders.de>
2019-11-28 00:37:19 +0100

    Move start section to separate component
    Signed-off-by: Till Sanders <mail@till-sanders.de>

---
+++ src/components/StartSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container mx-auto px-4 flex flex-col h-screen content-center justify-center">
+    <img
+      src="../assets/logo.svg"
+      alt="Open Election Compass"
+      style="width:100px;"
+      class="self-center mb-24"
+    />
+    <h1 class="text-5xl text-center">
+      {{ $t('title') }}<br>
+      <small>{{ $t('subtitle') }}</small>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StartSection',
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,16 @@
 <template>
-  <div class="container mx-auto px-4 flex flex-col h-screen content-center justify-center">
-    <img
-      src="../assets/logo.svg"
-      alt="Open Election Compass"
-      style="width:100px;"
-      class="self-center mb-24"
-    />
-    <h1 class="text-5xl text-center">
-      {{ $t('title') }}<br>
-      <small>{{ $t('subtitle') }}</small>
-    </h1>
+  <div>
+    <start-section />
   </div>
 </template>
 
 <script>
+import StartSection from '@/components/StartSection.vue';
+
 export default {
   name: 'home',
+  components: {
+    StartSection,
+  },
 };
 </script>
