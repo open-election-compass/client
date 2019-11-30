aad8f9475ef0fdaf9b52ef163f7c0e9dd082dfdb
Till Sanders <mail@till-sanders.de>
2019-11-30 06:47:29 +0100

    Improve StartSection
    Signed-off-by: Till Sanders <mail@till-sanders.de>

--- src/components/StartSection.vue
+++ src/components/StartSection.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class="container mx-auto px-4 flex flex-col h-screen content-center justify-center">
+  <div class="container mx-auto p-8 flex flex-col h-screen content-center justify-around">
     <img
       src="../assets/logo.svg"
       alt="Open Election Compass"
-      style="width:100px;"
-      class="self-center mb-24"
+      style="width: 30%; max-width:100px;"
+      class="self-center"
     />
-    <h1 class="text-5xl text-center">
-      {{ $t('title') }}<br>
-      <small>{{ $t('subtitle') }}</small>
+    <h1 class="text-2xl leading-tight text-center">
+      {{ $t('title') }}
     </h1>
+    <a class="bg-primary text-color-white p-4 rounded inline-block self-center">Los geht's</a>
+    <p class="text-sm text-center text-gray-500">{{ $t('subtitle') }}</p>
   </div>
 </template>
 
