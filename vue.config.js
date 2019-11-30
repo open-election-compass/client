83bc395341b296d14dc8ec9e21b6de5e23cb1099
Till Sanders <mail@till-sanders.de>
2019-11-30 06:46:27 +0100

    Install vue-i18n
    Signed-off-by: Till Sanders <mail@till-sanders.de>

--- vue.config.js
+++ vue.config.js
@@ -8,4 +8,13 @@ module.exports = {
       enableInSFC: false,
     },
   },
+  chainWebpack: (config) => {
+    config.module
+      .rule('i18n')
+      .resourceQuery(/blockType=i18n/)
+      .type('javascript/auto')
+      .use('i18n')
+      .loader('@kazupon/vue-i18n-loader')
+      .end();
+  },
 };
