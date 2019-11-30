31a778326e8180b83b44dae9b21bb94638d7d3ad
Till Sanders <mail@till-sanders.de>
2019-11-30 06:47:03 +0100

    Improve IntroductionSection
    Signed-off-by: Till Sanders <mail@till-sanders.de>

--- src/components/IntroductionSection.vue
+++ src/components/IntroductionSection.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="container max-w-2xl mx-auto mt-32 mb-32"
+    class="container max-w-2xl mx-auto p-8 bg-gray-200 text-gray-600"
   >
-    <h2 class="text-3xl text-primary font-bold mb-10">
+    <h2 class="text-2xl leading-tight font-bold mb-10">
       {{ $t('introduction.heading') }}
     </h2>
     <p>
