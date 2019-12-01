27fe4bb2089bfe1554ed799b12e5d865a04eb932
Till Sanders <mail@till-sanders.de>
2019-12-01 22:46:05 +0100

    Improve layout of IntroductionSection
    Signed-off-by: Till Sanders <mail@till-sanders.de>

--- src/components/IntroductionSection.vue
+++ src/components/IntroductionSection.vue
@@ -1,13 +1,19 @@
 <template>
-  <div
-    class="container max-w-2xl mx-auto p-8 bg-gray-200 text-gray-600"
-  >
-    <h2 class="text-2xl leading-tight font-bold mb-10">
-      {{ $t('introduction.heading') }}
-    </h2>
-    <p>
-      {{ $t('introduction.text') }}
-    </p>
+  <div class="bg-gray-200">
+    <div
+      class="
+        container max-w-2xl mx-auto p-8 text-gray-600
+        sm:py-16
+        lg:py-24
+      "
+    >
+      <h2 class="text-2xl leading-tight font-bold mb-10">
+        {{ $t('introduction.heading') }}
+      </h2>
+      <p>
+        {{ $t('introduction.text') }}
+      </p>
+    </div>
   </div>
 </template>
 
