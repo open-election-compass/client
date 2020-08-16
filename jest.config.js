a61883212a1d7fd62165b63ec28afe147fdefb68
Till Sanders <mail@till-sanders.de>
2020-08-16 14:52:24 +0200

    ci: set up coverage collection
    Signed-off-by: Till Sanders <mail@till-sanders.de>

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,vue}',
+    '!**/*.config.js',
+    '!**/coverage/**',
+    '!**/dist/**',
+    '!**/node_modules/**',
+    '!**/tests/**',
+  ],
 };
